9b7f5c55ececda7db18bd22a78fdc8511a34fd13
OMaloso <88115296+OMaloso@users.noreply.github.com>
2022-06-09 22:27:09 +1200

    Updated hallway scene to utilise new background artwork. Updated lighting and collisions to fit. Added new collisions at ends of hallway. Changed player movement script to check for current scene, locking vertical movement if in hallway scene.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Scene currentScene; 
+
     Rigidbody2D PlayerBody;
 
     float fHorizontal;
@@ -14,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         PlayerBody = GetComponent<Rigidbody2D>();
+        currentScene = SceneManager.GetActiveScene(); // Get current scene and store in variable
     }
 
     void Update()
@@ -24,10 +28,13 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        PlayerBody.velocity = new Vector2(fHorizontal * fRunSpeed, fVertical * fRunSpeed);
+        if (currentScene.name == "HallwayPrototypeScene") // Check if current scene is the hallway scene
+        {
+            PlayerBody.velocity = new Vector2(fHorizontal * fRunSpeed, fVertical * 0); // if it is, then lock vertical movement
+        }
+        else
+        {
+            PlayerBody.velocity = new Vector2(fHorizontal * fRunSpeed, fVertical * fRunSpeed); // otherwise, allow vertical movement
+        }
     }
-
-
-
-
 }
